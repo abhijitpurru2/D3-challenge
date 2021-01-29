95fa8c83393eff0ca47e899ffb30ba95c39fdc05
abhijitpurru2 <68308831+abhijitpurru2@users.noreply.github.com>
2021-01-28 21:28:03 -0500

    Update app.js

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,6 +1,6 @@
 
-var svgWidth = 850;
-var svgHeight = 600;
+var svgWidth = 960;
+var svgHeight = 500;
 
 var margin = {
       top: 20,
@@ -14,7 +14,7 @@ var svgScatter = d3.select("#scatter")
                    .attr("width", svgWidth)
                    .attr("height", svgHeight);
 
-var chartLocal = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
+var chartLocal = svgScatter.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.csv("assets/data/data.csv").then(function(dataSet){
     dataSet.forEach(function(data){
@@ -24,6 +24,7 @@ d3.csv("assets/data/data.csv").then(function(dataSet){
         data.healthcare = +data.healthcare;
         data.smokes = +data.smokes;
         data.obesity = +data.obesity;
+        data.abbr = +data.abbr;
 
     var width = svgWidth - margin.left - margin.right;
     var height = svgHeight - margin.top - margin.bottom;
@@ -34,10 +35,38 @@ d3.csv("assets/data/data.csv").then(function(dataSet){
 
     var yScale = d3.scaleLinear()
                    .domain([d3.min(dataSet, d => d.healthcare),d3.max(dataSet, d => d.healthcare)])
-                   .range([0, width])
+                   .range([height, 0])
 
     xAxis = d3.axisBottom(xScale);
     yAxis = d3.axisLeft(yScale);
+
+    chartLocal.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+
+    chartLocal.append("g")
+        .classed("y-axis", true)
+        .call(yAxis);
+
+    var circlesLocal = chartLocal.selectAll("circle")
+        .data(dataSet)
+        var element = circlesLocal.enter();
+
+
+
+    chartLocal.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - 50)
+            .attr("x", 0 -250)
+            .attr("dy", "1em")
+            .attr("class", "axisText")
+            .text("Healthcare Lack Percent (%)");
+
+        chartLocal.append("text")
+            .attr("transform", `translate(${width / 2.5}, ${height + margin.top + 25})`)
+            .attr("class", "axisText")
+            .text("Poverty Percent (%)");
 
     });
 });
